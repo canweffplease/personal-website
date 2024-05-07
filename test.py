bf9cd440fc948b784d343f33529b3163130bea2e
canweffplease <chrisfdas1@gmail.com>
2024-05-07 15:22:48 -0400

    edited website contents

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print(range(5))
+def test(nums, k):
+    current = 0
+    for i in range(k):
+        current += nums[i]
+    ans = current
+
+    for i in range(k, len(nums)): 
+        current += nums[i] - nums[i-k]
+        ans = max(ans, current)
+    return ans/k
+print(test([1,12,-5,-6,50,3],4))
